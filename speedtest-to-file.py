c710db8787e072748e932f7f6564697581145f12
jlosier <osierjos@gmail.com>
2017-12-29 15:36:05 -0500

    formatted code for PEP8

--- speedtest-to-file.py
+++ speedtest-to-file.py
@@ -13,10 +13,12 @@ try:
     filePath = sys.argv[2]
     fileFormat = sys.argv[2][-4:]
 except IndexError:
-    print("Cannot run. Improper arguments supplied.\nExample1: $ python *.py <verbose: true/false> <file path>\nExample2: $ python *.py true /home/pi/Documents/speedtest/speedtest.json")
+    print("Cannot run. Improper arguments supplied.\nExample1: "
+          "$ python *.py <verbose: true/false> <file path>\nExample2: "
+          "$ python *.py true /home/pi/Documents/speedtest/speedtest.json")
     sys.exit()
 
-if (len(sys.argv) > 2) :
+if (len(sys.argv) > 2):
     if (verbose == "TRUE"):
         verbose = True
     else:
@@ -25,18 +27,22 @@ if (len(sys.argv) > 2) :
     if ("." in fileFormat):
         fileFormat = "csv"
     else:
-        fileFormat = "json"      
+        fileFormat = "json"
 else:
-    print("Cannot run. Improper arguments supplied.\nExample1: $ python *.py <verbose: true/false> <file path>\nExample2: $ python *.py true /home/pi/Documents/speedtest/speedtest.json")
+    print("Cannot run. Improper arguments supplied.\nExample1: "
+          "$ python *.py <verbose: true/false> <file path>\nExample2: "
+          "$ python *.py true /home/pi/Documents/speedtest/speedtest.json")
     sys.exit()
 
+
 # print message defined in call if verbose == True
-def writeMessage (message, verbose) :
+def writeMessage(message, verbose):
     if verbose:
         print(message)
-    return;
+    return
 
-def writeToJson (results, filePath) :
+
+def writeToJson(results, filePath):
     file = Path(filePath)
 
     newJson = []
@@ -47,37 +53,46 @@ def writeToJson (results, filePath) :
 
             for entry in data:
                 newJson.append({
-                    'timestamp' : entry['timestamp'],
-                    'location' : entry['location'],
-                    'ping' : entry['ping'],
-                    'download' : entry['download'],
-                    'upload' : entry['upload']
+                    'timestamp': entry['timestamp'],
+                    'location': entry['location'],
+                    'ping': entry['ping'],
+                    'download': entry['download'],
+                    'upload': entry['upload']
                 })
 
         newJson.append({
-            'timestamp' : results[0]['timestamp'],
-            'location' : results[0]['location'],
-            'ping' : results[0]['ping'],
-            'download' : results[0]['download'],
-            'upload' : results[0]['upload']
+            'timestamp': results[0]['timestamp'],
+            'location': results[0]['location'],
+            'ping': results[0]['ping'],
+            'download': results[0]['download'],
+            'upload': results[0]['upload']
         })
-        
+
         with open(filePath, 'w') as write:
             json.dump(newJson, write, indent=4)
             write.close()
     except IOError as (errno, strerror):
         print("I/O error({0}): {1}".format(errno, strerror))
     else:
-        writeMessage(message = "Results successfully written to json file", verbose = verbose)
+        writeMessage(message="Results successfully written to json file",
+                     verbose=verbose)
+
 
 def writeToCsv(results, filePath):
     file = Path(filePath)
-    headings = ['timestamp','location','ping (ms)','download (Mbit/s)','upload (Mbit/s)']
-    
+    headings = ['timestamp',
+                'location',
+                'ping (ms)',
+                'download (Mbit/s)',
+                'upload (Mbit/s)']
+
     if (file.exists()):
         try:
-            with open(filePath,'a') as csvFile:
-                csvWriter = csv.DictWriter(csvFile, fieldnames=headings, dialect='excel', quoting=csv.QUOTE_NONNUMERIC)
+            with open(filePath, 'a') as csvFile:
+                csvWriter = csv.DictWriter(csvFile,
+                                           fieldnames=headings,
+                                           dialect='excel',
+                                           quoting=csv.QUOTE_NONNUMERIC)
                 for data in results:
                     csvWriter.writerow(data)
         except IOError as (errno, strerror):
@@ -85,8 +100,11 @@ def writeToCsv(results, filePath):
         return
     else:
         try:
-            with open(filePath,'w') as csvFile:
-                csvWriter = csv.DictWriter(csvFile, fieldnames=headings, dialect='excel', quoting=csv.QUOTE_NONNUMERIC)
+            with open(filePath, 'w') as csvFile:
+                csvWriter = csv.DictWriter(csvFile,
+                                           fieldnames=headings,
+                                           dialect='excel',
+                                           quoting=csv.QUOTE_NONNUMERIC)
                 csvWriter.writeheader()
                 for data in results:
                     csvWriter.writerow(data)
@@ -95,11 +113,11 @@ def writeToCsv(results, filePath):
         return
 
 # start the test
-writeMessage(message = "Performing speed test. Please wait...", verbose = verbose)
+writeMessage(message="Performing speed test. Please wait...", verbose=verbose)
 
 speed = os.popen("/usr/local/bin/speedtest-cli").read()
 
-writeMessage(message = "Complete! Here are your results:", verbose = verbose)
+writeMessage(message="Complete! Here are your results:", verbose=verbose)
 
 if ("Cannot" not in speed):
     # handle the results
@@ -109,32 +127,36 @@ if ("Cannot" not in speed):
     ping = location[1].split(' ')
     download = lines[6].split(' ')
     upload = lines[8].split(' ')
-        
+
 else:
     # error occurred while running speed test
-    writeMessage(message = "There was a problem getting results from speedtest-cli", verbose = verbose)
+    writeMessage(message="There was a problem getting results from "
+                         "speedtest-cli", verbose=verbose)
     location = ["ERROR"]
     ping = ["ERROR", "0"]
     download = ["ERROR", "0"]
     upload = ["ERROR", "0"]
 
 results = [{
-        'timestamp' : time.strftime("%Y-%m-%d %X"),
-        'location' : location[0],
-        'ping' : ping[1], 
-        'download' : download[1],
-        'upload' : upload[1]
+        'timestamp': time.strftime("%Y-%m-%d %X"),
+        'location': location[0],
+        'ping': ping[1],
+        'download': download[1],
+        'upload': upload[1]
     }]
 
-writeMessage(message = "Timestamp: " + results[0]['timestamp'], verbose = verbose)
-writeMessage(message = "Location: " + results[0]['location'], verbose = verbose)
-writeMessage(message = "Ping (ms): " + results[0]['ping'], verbose = verbose)
-writeMessage(message = "Download (Mbit/s): " + results[0]['download'], verbose = verbose)
-writeMessage(message = "Upload (Mbit/s): " + results[0]['upload'], verbose = verbose)
+writeMessage(message="Timestamp: " + results[0]['timestamp'],
+             verbose=verbose)
+writeMessage(message="Location: " + results[0]['location'],
+             verbose=verbose)
+writeMessage(message="Ping (ms): " + results[0]['ping'],
+             verbose=verbose)
+writeMessage(message="Download (Mbit/s): " + results[0]['download'],
+             verbose=verbose)
+writeMessage(message="Upload (Mbit/s): " + results[0]['upload'],
+             verbose=verbose)
 
 if (fileFormat == "json"):
-    writeToJson(results = results, filePath = filePath)
+    writeToJson(results=results, filePath=filePath)
 else:
-    writeToCsv(results = results, filePath = filePath)
-
-    
+    writeToCsv(results=results, filePath=filePath)
